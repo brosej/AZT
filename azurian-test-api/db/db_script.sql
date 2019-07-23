8f5d83b227cf5254e69ca89020ccb26df2acfff8
Jose Bermudez <brosejr20@gmail.com>
2019-07-23 19:01:12 -0400

    fix db script

--- azurian-test-api/db/db_script.sql
+++ azurian-test-api/db/db_script.sql
@@ -1,8 +1,8 @@
 CREATE TABLE azurian_products (
                 id INT AUTO_INCREMENT NOT NULL,
-                name VARCHAR NOT NULL,
-                price DOUBLE PRECISIONS NOT NULL,
-                description VARCHAR NOT NULL,
-                department VARCHAR NOT NULL,
+                name VARCHAR(50) NOT NULL,
+                price DOUBLE NOT NULL,
+                description VARCHAR(50) NOT NULL,
+                department VARCHAR(50) NOT NULL,
                 PRIMARY KEY (id)
 );
